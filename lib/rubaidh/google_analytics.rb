5e90760a125c9d24d2e000b2b54d02ddacda43dd
Graeme Mathieson <mathie@woss.name>
2006-05-10 12:01:37 +0000

    * Add (untested, since I don't have an SSL host set up!) support for using   the SSL analytics URL to avoid having a mixture of SSL and non-SSL portions   of a page. * Add in support for specifying the _udn which is required for my own   web site.
    git-svn-id: https://svn.rubaidh.com/plugins/trunk/google_analytics@16 d6511e60-4710-0410-a7ce-ed01584c9eb4

--- lib/rubaidh/google_analytics.rb
+++ lib/rubaidh/google_analytics.rb
@@ -1,13 +1,13 @@
 module Rubaidh # :nodoc:
   module GoogleAnalyticsMixin
-    def google_analytics_code
+    def google_analytics_code(request = nil)
       return unless GoogleAnalytics.enabled?
-      GoogleAnalytics.new.google_analytics_code
+      GoogleAnalytics.new.google_analytics_code(request)
     end
     
     # An after_filter to automatically add the analytics code.
     def add_google_analytics_code
-      code = google_analytics_code
+      code = google_analytics_code(request)
       return if code.blank?
       response.body.gsub! '</body>', code + '</body>'
     end
@@ -19,10 +19,22 @@ module Rubaidh # :nodoc:
     @@tracker_id = nil
     cattr_accessor :tracker_id
 
+    # Specify a different domain name from the default.  You'll want to use
+    # this if you have several subdomains that you want to combine into
+    # one report.  See the Google Analytics documentation for more
+    # information.
+    @@domain_name = nil
+    cattr_accessor :domain_name
+    
     # I can't see why you'd want to do this, but you can always change the
     # analytics URL.
     @@analytics_url = 'http://www.google-analytics.com/urchin.js'
     cattr_accessor :analytics_url
+
+    # I can't see why you'd want to do this, but you can always change the
+    # analytics URL (ssl version).
+    @@analytics_ssl_url = 'https://www.google-analytics.com/urchin.js'
+    cattr_accessor :analytics_ssl_url
 
     # The environments in which to enable the Google Analytics code.  Defaults
     # to 'production' only.
@@ -37,14 +49,19 @@ module Rubaidh # :nodoc:
         not analytics_url.blank?)
     end
     
-    def google_analytics_code
+    def google_analytics_code(request = nil)
+      extra_code = domain_name.blank? ? nil : "_udn = \"#{domain_name}\";"
+      breakpoint
+      url = (not request.blank? and request.ssl?) ? analytics_ssl_url : analytics_url
+
       # OK, I'm not very bright -- I tried to turn this into a partial and
       # failed miserably!  So it'll have to live here for now.
       code = <<-HTML
-      <script src="#{analytics_url}" type="text/javascript">
+      <script src="#{url}" type="text/javascript">
       </script>
       <script type="text/javascript">
       _uacct = "#{tracker_id}";
+      #{extra_code}
       urchinTracker();
       </script>
       HTML
