685370235df009d0a4000cd1f66da3050b5d9761
Mike Gunderloy <MikeG1@larkfarm.com>
2008-08-22 19:57:53 -0500

    DRY up init.rb

--- init.rb
+++ init.rb
@@ -1,6 +1,1 @@
-require 'rubaidh/google_analytics'
-require 'rubaidh/view_helpers'
-ActionController::Base.send :include, Rubaidh::GoogleAnalyticsMixin
-ActionController::Base.send :after_filter, :add_google_analytics_code
-ActionView::Base.send :include, Rubaidh::GoogleAnalyticsViewHelper
- 
+require File.dirname(__FILE__) + "/rails/init"
